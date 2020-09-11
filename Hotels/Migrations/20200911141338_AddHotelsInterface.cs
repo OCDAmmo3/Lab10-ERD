a0172a0f4e1de1c6427cf5f03e4ae7ed605feccc
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-11 11:35:22 -0500

    Added hotel interface migration

---
+++ Hotels/Migrations/20200911141338_AddHotelsInterface.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotels.Migrations
+{
+    public partial class AddHotelsInterface : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Hotels",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "City", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[] { 1L, "Cedar Rapids", "Marriot CR", "3193730354", "IA", "3350 26th Ave" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Phone",
+                table: "Hotels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- Hotels/Startup.cs
+++ Hotels/Startup.cs
@@ -40,6 +40,7 @@ namespace Hotels
             });
 
             services.AddTransient<IRoomRepository, DatabaseRoomRepository>();
+            services.AddTransient<IHotelRepository, DatabaseHotelRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
