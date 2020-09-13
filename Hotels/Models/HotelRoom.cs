a4210e51409833ebd10e3acfcf3e02bbd81560b6
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-13 10:49:08 -0500

    Added hotelRoom model

---
+++ Hotels/Models/HotelRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace Hotels.Models
+{
+    public class HotelRoom
+    {
+        public long HotelId { get; set; }
+        public long RoomId { get; set; }
+        public Hotel Hotel { get; set; }
+        public Room Room { get; set; }
+
+        public int RoomNumber { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+    }
+}
