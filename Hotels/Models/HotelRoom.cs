5cb14d984c65f190254c9390e170f75e317d1469
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-14 16:46:27 -0500

    Rearranged HotelRoom model to accurately represent composite key

--- Hotels/Models/HotelRoom.cs
+++ Hotels/Models/HotelRoom.cs
@@ -2,13 +2,12 @@
 {
     public class HotelRoom
     {
-        public long Id { get; set; }
         public long HotelId { get; set; }
-        public long RoomId { get; set; }
+        public int RoomNumber { get; set; }
         public Hotel Hotel { get; set; }
         public Room Room { get; set; }
 
-        public int RoomNumber { get; set; }
+        public long RoomId { get; set; }
         public decimal Rate { get; set; }
         public bool PetFriendly { get; set; }
     }
