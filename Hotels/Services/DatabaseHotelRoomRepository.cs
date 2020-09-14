6a71e0c43c7286f306fb11c2355a7bce0562a2e6
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-14 18:32:07 -0500

    Working implementation to get a single hotel room

--- Hotels/Services/DatabaseHotelRoomRepository.cs
+++ Hotels/Services/DatabaseHotelRoomRepository.cs
@@ -16,14 +16,26 @@ namespace Hotels.Services
             _context = context;
         }
 
-        public async Task<IEnumerable<HotelRoom>> GetAllAsync()
-        {
-            return await _context.HotelRooms.ToListAsync();
-        }
-
         public async Task<IEnumerable<HotelRoom>> GetHotelRoomsById(long hotelId)
         {
             var hotelRoom = await _context.HotelRooms.Where(hr => hr.HotelId == hotelId).ToListAsync();
+            return hotelRoom;
+        }
+
+        public async Task<HotelRoom> GetHotelRoomAsync(long hotelId, int roomNumber)
+        {
+            var hotelRoom = await _context.HotelRooms
+                .Where(hr => hr.HotelId == hotelId)
+                .Where(h => h.RoomNumber == roomNumber)
+                .Include(hr => hr.Hotel)
+                .ThenInclude(h => h.HotelRooms)
+                .Include(hr => hr.Room)
+                .ThenInclude(r => r.HotelRooms)
+                .Include(hr => hr.Room)
+                .ThenInclude(r => r.RoomAmenities)
+                .ThenInclude(ra => ra.Amenity)
+                .ThenInclude(a => a.RoomAmenities)
+                .FirstOrDefaultAsync();
             return hotelRoom;
         }
 
--- Hotels/Services/IHotelRoomRepository.cs
+++ Hotels/Services/IHotelRoomRepository.cs
@@ -9,7 +9,7 @@ namespace Hotels.Services
         Task<IEnumerable<HotelRoom>> GetHotelRoomsById(long hotelId);
         Task AddRoomAsync(long hotelId, CreateHotelRoom hotelRoom);
         Task DeleteRoomAsync(long hotelId, int roomNumber);
-        Task<IEnumerable<HotelRoom>> GetAllAsync();
         Task<bool> UpdateAsync(HotelRoom hotelRoom);
+        Task<HotelRoom> GetHotelRoomAsync(long hotelId, int roomNumber);
     }
 }
