13547172f9ff7850dad08848418ee7edf2f17734
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-17 18:48:53 -0500

    Hotel rooms add and delete tested

---
+++ Hotels.Tests/Services/DatabaseHotelRepositoryTests.cs
@@ -0,0 +1,74 @@
+﻿using Hotels.Models;
+using Hotels.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hotels.Tests.Services
+{
+    public class DatabaseHotelRepositoryTests : DatabaseTestBase
+    {
+        [Fact]
+        public async Task Can_add_and_remove_amenity_to_room()
+        {
+            // Arrange
+            var room = await CreateAndSaveTestRoom();
+            var amenity = await CreateAndSaveTestAmenity();
+
+            var repository = new DatabaseRoomRepository(_db);
+
+            // Act
+            await repository.AddAmenityAsync(
+                amenityId: amenity.Id,
+                roomId: room.Id
+                );
+
+            // Assert
+            var realRoom = await repository.GetOneByIdAsync(room.Id);
+            Assert.Contains(realRoom.RoomAmenities, ra => ra.Amenity.Name == "Jacuzzi");
+
+            // Act
+            await repository.DeleteAmenityAsync(
+                amenityId: amenity.Id,
+                roomId: room.Id);
+
+            // Assert
+            realRoom = await repository.GetOneByIdAsync(room.Id);
+            Assert.DoesNotContain(realRoom.RoomAmenities, ra => ra.Amenity.Name == "Jacuzzi");
+        }
+
+        [Fact]
+        public async Task Can_add_and_remove_room_to_hotel()
+        {
+            // Arrange
+            var room = await CreateAndSaveTestRoom();
+            var hotel = await CreateAndSaveTestHotel();
+            var createRoom = new CreateHotelRoom() { RoomNumber = 14, Rate = 149.99m, PetFriendly = true, RoomId = room.Id };
+
+            var repository = new DatabaseHotelRoomRepository(_db);
+
+            // Act
+            await repository.AddRoomAsync(
+                hotelId: hotel.Id,
+                hotelRoom: createRoom
+                );
+
+            // Assert
+            var realHotelRoom = await repository.GetHotelRoomAsync(hotel.Id, createRoom.RoomNumber);
+            Assert.Equal(14, realHotelRoom.RoomNumber);
+            Assert.True(realHotelRoom.PetFriendly);
+
+            // Act
+            await repository.DeleteRoomAsync(
+                hotelId: hotel.Id,
+                roomNumber: createRoom.RoomNumber
+                );
+
+            // Assert
+            realHotelRoom = await repository.GetHotelRoomAsync(hotel.Id, createRoom.RoomNumber);
+            Assert.Null(realHotelRoom);
+        }
+    }
+}
---
+++ Hotels.Tests/Services/DatabaseTestBase.cs
@@ -0,0 +1,63 @@
+﻿using Hotels.Data;
+using Hotels.Models;
+using Hotels.Services;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hotels.Tests.Services
+{
+    public class DatabaseTestBase : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        protected readonly HotelDbContext _db;
+
+        public DatabaseTestBase()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            _db = new HotelDbContext(
+                new DbContextOptionsBuilder<HotelDbContext>()
+                    .UseSqlite(_connection)
+                    .Options);
+
+            _db.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _db?.Dispose();
+            _connection?.Dispose();
+        }
+
+        protected async Task<Room> CreateAndSaveTestRoom()
+        {
+            var room = new Room { Name = "Studio Room", Layout = Room.RoomLayout.Studio };
+            _db.Rooms.Add(room);
+            await _db.SaveChangesAsync();
+            Assert.NotEqual(0, room.Id);
+            return room;
+        }
+
+        protected async Task<Amenity> CreateAndSaveTestAmenity()
+        {
+            var amenity = new Amenity { Name = "Jacuzzi" };
+            _db.Amenities.Add(amenity);
+            await _db.SaveChangesAsync();
+            Assert.NotEqual(0, amenity.Id);
+            return amenity;
+        }
+
+        protected async Task<Hotel> CreateAndSaveTestHotel()
+        {
+            var hotel = new Hotel { Name = "Marriot", City = "Cedar Rapids", State = "IA", StreetAddress = "1234 5th St" };
+            _db.Hotels.Add(hotel);
+            await _db.SaveChangesAsync();
+            Assert.NotEqual(0, hotel.Id);
+            return hotel;
+        }
+    }
+}
