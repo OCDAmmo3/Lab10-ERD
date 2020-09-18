8eea4f224476192d380a93d6fc6a312513623d5a
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-18 10:53:48 -0500

    Updated database for hotelRoom

---
+++ Hotels/Migrations/20200918155232_DTOUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotels.Migrations
+{
+    public partial class DTOUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "HotelRooms");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Id",
+                table: "HotelRooms",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
