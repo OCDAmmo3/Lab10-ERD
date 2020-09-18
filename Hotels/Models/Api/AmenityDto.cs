9f5df0f94e699844f12ad1f5da08b52520533670
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-18 08:03:15 -0500

    Both get methods for Amenities going through Dto

---
+++ Hotels/Models/Api/AmenityDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Hotels.Models.Api
+{
+    public class AmenityDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class RoomAmenityDto
+    {
+        public long RoomId { get; set; }
+        public long AmenityId { get; set; }
+        public Room Room { get; set; }
+        public Amenity Amenity { get; set; }
+    }
+}
